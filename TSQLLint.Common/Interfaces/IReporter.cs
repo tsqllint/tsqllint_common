253679884ff0ed1b48974bf881f89892d270a629
Dan Gasperut <dgasperut@salesforce.com>
2018-01-24 15:43:36 -0600

    adding interface to reporter

--- TSQLLint.Common/Interfaces/IReporter.cs
+++ TSQLLint.Common/Interfaces/IReporter.cs
@@ -5,6 +5,7 @@ namespace TSQLLint.Common
     public interface IReporter : IBaseReporter
     {
         void ReportResults(TimeSpan timespan, int fileCount);
+        void ReportFileResults();
         void ReportViolation(IRuleViolation violation);
         void ReportViolation(string fileName, string line, string column, string severity, string ruleName, string violationText);
     }

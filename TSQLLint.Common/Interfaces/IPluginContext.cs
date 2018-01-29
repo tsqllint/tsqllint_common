10572fce9d843e479dc206c482721ac56269e054
Nathan Boyd <nathan@engineernathan.org>
2018-01-29 15:05:05 -0500

    Update Plugin Context to Require List of Rule Exceptions

--- TSQLLint.Common/Interfaces/IPluginContext.cs
+++ TSQLLint.Common/Interfaces/IPluginContext.cs
@@ -1,10 +1,14 @@
-﻿using System.IO;
+﻿using System.Collections.Generic;
+using System.IO;
 
 namespace TSQLLint.Common
 {
     public interface IPluginContext
     {
         string FilePath { get; }
+        
         TextReader FileContents { get; }
+
+        IEnumerable<IRuleException> RuleExceptions { get; }
     }
 }

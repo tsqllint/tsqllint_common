041c7649a9473a9259cf9848012ab9817817506c
Nathan Boyd <nboyd@salesforce.com>
2021-08-29 17:05:36 -0400

    consolidate release and ci scripts

--- scripts/ci_run.sh
+++ scripts/ci_run.sh
@@ -27,3 +27,39 @@ echoBlockMessage "running test project"
 dotnet test \
     --no-restore \
     ./TSQLLint.Common.Tests/TSQLLint.Common.Tests.csproj
+
+echoBlockMessage "packing project"
+
+dotnet pack \
+    ./TSQLLint.Common/TSQLLint.Common.csproj \
+    -p:VERSION="$VERSION" \
+    --configuration Release \
+    --output /artifacts
+
+if [ "$RELEASE" == "false" ]; then
+    echoMessage "done"
+    exit 0
+fi
+
+echoBlockMessage "releasing project"
+
+if [[ -z "$NUGET_API_KEY" ]]; then
+    echoMessage "NUGET_API_KEY is not set in the environment."
+    echoMessage "Artifacts will not be pushed to Nuget."
+    exit 1
+fi
+
+echoBlockMessage "pushing artifacts to Nuget"
+
+dotnet nuget push \
+    "/artifacts/TSQLLint.Common.$VERSION.nupkg" \
+    --api-key "$NUGET_API_KEY"  \
+    --source https://api.nuget.org/v3/index.json
+
+echoBlockMessage "creating release in github"
+
+gh auth login  --hostname "github.com" --with-token < "$GITHUB_TOKEN_FILE"
+
+gh release create "$VERSION" -d /artifacts/*.nupkg
+
+echoMessage "done"

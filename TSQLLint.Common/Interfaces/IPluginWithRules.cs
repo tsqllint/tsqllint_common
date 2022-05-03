364a7a559bece2f5e665fa6af861a025cf4fe0af
Stephen Weaver <stephen.weaver@salesforce.com>
2022-05-03 09:21:50 -0600

    Add IPluginWithRules for --fix support

---
+++ TSQLLint.Common/Interfaces/IPluginWithRules.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace TSQLLint.Common
+{
+    public interface IPluginWithRules : IPlugin
+    {
+        /// <summary>
+        /// Required if you
+        /// </summary>
+        Dictionary<string, ISqlLintRule> Rules { get; }
+    }
+}
---
+++ TSQLLint.Common/Interfaces/ISqlRule.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace TSQLLint.Common
+{
+    public interface ISqlLintRule
+    {
+        string RULE_NAME { get; }
+
+        public void FixViolation(List<string> fileLines, IRuleViolation ruleViolation);
+    }
+}

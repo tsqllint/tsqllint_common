7699998092570cb05b0e69c5b66fdd8053773b99
Lawrence Tylenda <ltylenda@salesforce.com>
2022-07-20 11:52:09 -0400

    Updated IPlugin to have method GetRules with default method. Added backwards compatibility when using IPluginWithRules and the new IPlugin.GetRules method. Deprecated IPluginWithRules.

--- TSQLLint.Common.Tests/UnitTests/TestPlugin.cs
+++ TSQLLint.Common.Tests/UnitTests/TestPlugin.cs
@@ -1,13 +1,21 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace TSQLLint.Common.Tests
 {
     public class TestPlugin : IPlugin
     {
-
         public void PerformAction(IPluginContext context, IReporter reporter)
         {
             throw new NotImplementedException();
         }
+    }
+
+    // TODO: Remove this class when removing IPluginWithRules
+    public class TestPluginWithRules : IPluginWithRules
+    {
+        public void PerformAction(IPluginContext context, IReporter reporter) => throw new NotImplementedException();
+
+        public Dictionary<string, ISqlLintRule> Rules => throw new NotImplementedException();
     }
 }
